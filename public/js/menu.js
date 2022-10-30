7ae91c589b005ac4640153769384cd347f4fcd01
jmrxx <thos.josemejia@gmail.com>
2022-10-30 13:49:18 +0100

    mobile-falta

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,6 +1,7 @@
 
 const btn = document.querySelector("#btn");
 	const menu = document.querySelector(".menu");
+	const heigth = document.querySelector(".nav");
 
 	btn.addEventListener("click", () => {
 		menu.classList.toggle("active");
