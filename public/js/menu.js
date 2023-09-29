6171fc014b1e3f2d5f9c9a60b3b11f6fe3086ffb
jmrxx <thos.josemejia@gmail.com>
2023-09-29 17:19:32 +0200

    add variable in css

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,9 +1,17 @@
-
+/* show menu mobil */
 const btn = document.querySelector("#btn");
-	const menu = document.querySelector(".menu");
-	const heigth = document.querySelector(".nav");
+const menu = document.querySelector(".ul");
+const heigth = document.querySelector(".nav");
 
-	btn.addEventListener("click", () => {
-		menu.classList.toggle("active");
-	});
+btn.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
 
+/* current link nav active*/
+const navbar = document.querySelector("#menu").querySelectorAll("a");
+navbar.forEach((element) => {
+  element.addEventListener("click", function () {
+    navbar.forEach((nav) => nav.classList.remove("current"));
+    this.classList.add("current");
+  });
+});
---
+++ readme.md
@@ -0,0 +1,25 @@
+# UF1. Disseny de la interfície
+
+## Pràctica 1. Disseny d'interfícies multipantalla. Responsive web design
+
+profesor : Alfredo Ballestero Andreu - Departament d'Informàtica
+
+Alumno: Jose jerlin Mejia Sanchez
+
+---
+
+ wireframe --> public/wireframe
+
+ abstracts/_mixins.scss
+
+layout/_grid.scss
+
+
+
+# pasos: 
+1. estructura
+2. instalacion del sass,  livesasscompiler
+3. comprovar que el plugin funciona.
+4. estructura responsive  a layout/grid
+5. Variables todas en css
+
